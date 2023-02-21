7e2777861c66579071c3ec593ad84345f0a0dce9
Jorge Vasconcelos <jorge.r.p.v@hotmail.com>
2023-02-21 15:19:05 -0300

    feat: raise situation

--- main.py
+++ main.py
@@ -10,11 +10,20 @@ from utils import dict_to_str
 
 app = FastAPI()
 
+@tracer.start_as_current_span(name="do some stuff")
+def do_some_stuff(value: int):
+    if value == 3:
+        raise ValueError("isso é um erro")
+    return value
 
 def long_func():
     with tracer.start_as_current_span(name='long_func') as span:
         random_value = random.randint(1, 3)
+        span.set_attribute("long_func.random_value", random_value)
         time.sleep(random_value)
+
+        do_some_stuff(value=random_value)
+
         span.set_attribute("long_func.return", random_value)
         return random_value
 
@@ -24,11 +33,11 @@ def long_func():
 @tracer_endpoint()
 def home():
     r = long_func()
-    f_return = {'return': r}
-    return JSONResponse(content=f_return,status_code=200)
+    return JSONResponse(content={'return': r},status_code=200)
 
 
 FastAPIInstrumentor.instrument_app(app, tracer_provider=tracer_provider)
 uvicorn.run(
-    app=app
+    app=app,
+    port=8080
             )
